7ccb08f180105a90130ed9292f8600cfe5cac439
Lucas <lucas.rodrigues.ismart@gmail.com>
2022-09-23 16:05:53 -0300

    first commit

--- handler.py
+++ handler.py
@@ -1,10 +1,11 @@
+import os
 import pickle
 import pandas as pd
 from flask             import Flask, request, Response
 from rossmann.Rossmann import Rossmann
 
 # loading model
-model = pickle.load(open('/home/lucas/Desktop/repos/ds-em-producao/webapp/model/model_rossmann.pkl', 'rb'))
+model = pickle.load(open('/model/model_rossmann.pkl', 'rb'))
 
 # initialize API
 app = Flask(__name__)
@@ -42,4 +43,5 @@ def rossmann_predict():
         return Reponse('{}', status=200, mimetype='application/json')
 
 if __name__ == '__main__':
-    app.run('0.0.0.0')
+    port = os.environ.get('PORT', 5000)
+    app.run(host='0.0.0.0', port=port)
