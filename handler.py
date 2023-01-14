1e73a849eb3851641c54a47d32f1645418a29d41
Lucas <lucas.rodrigues.ismart@gmail.com>
2023-01-14 19:11:34 -0300

    Testando alteracao no handler.py

--- handler.py
+++ handler.py
@@ -10,7 +10,7 @@ model = pickle.load(open('model/model_rossmann.pkl', 'rb'))
 # initialize API
 app = Flask(__name__)
 
-@app.route('/rossmann/predict', methods=['POST'])
+@app.route('https://rossmann-api-62ta.onrender.com/rossmann/predict', methods=['POST'])
 def rossmann_predict():
     test_json = request.get_json()
    
